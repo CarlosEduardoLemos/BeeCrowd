42dbd20f106895ec055733d864a0dc69779715e8
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2024-04-13 12:29:30 -0300

    Update

---
+++ desafios/C/ex005.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int valor1, valor2, PROD;
+
+    // Lendo os dois valores inteiros
+    scanf("%d", &valor1);
+    scanf("%d", &valor2);
+
+    // Calculando o produto
+    PROD = valor1 * valor2;
+
+    // Imprimindo o resultado
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
---
+++ desafios/python/ex005.py
@@ -0,0 +1,9 @@
+# Lendo os dois valores inteiros
+valor1 = int(input())
+valor2 = int(input())
+
+# Calculando o produto
+PROD = valor1 * valor2
+
+# Imprimindo o resultado
+print("PROD =", PROD)
